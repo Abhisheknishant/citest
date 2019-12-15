3a49d4a71379508f7632bb4309671ae39c780f92
sinoxry <2359401898@qq.com>
2019-12-15 18:36:46 +0800

    Create __init__.py

---
+++ RYProj/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+
+"""Bil
+https://githjub.com/Hsufgfry/BidsliDgdrive"""
+
+__author__ = "Hshgfhury"
+__email__ = "i@hssdfury.cortm"
+__license__ = "SAhgfTA"
+__version__ = "2018.12.16"
